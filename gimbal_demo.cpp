7e994c259a77b9b0131f66ecee5e3d533ebca321
Angelos Anto <aggelos.anto@gmail.com>
2020-03-06 15:39:28 +0200

    Comments Added

--- gimbal_demo.cpp
+++ gimbal_demo.cpp
@@ -8,30 +8,38 @@
 #include <sstream>
 #include <string.h> 
 
-// Gimbal Includes
+// Gimbal Include
 #include "gimbal.h"
-
-//#define DBG
 
 using namespace std;
 
-
 int main(int argc, char **argv)
 {
-    
+    // Create gimbal instance
     Gimbal my_gimbal;
 
+    // Connect
     my_gimbal.connect_gimbal();
 
+    // Or Connect like that
+    //my_gimbal.connect_gimbal("/dev/ttyUSB1");
+
+    // Engage
     my_gimbal.engage_gimbal();
 
+    // You can set speed like that
     my_gimbal.set_gimbal_speed(0, 10, 0);
+    
+    // Wait 2 seconds (The gimbal is still moving)
     usleep(2000000);
 
+    // Set angle relative to frame like that
     my_gimbal.set_gimbal_angle(0.0, 0.0, 0.0);
+    
+    // Wait a while
     usleep(1000000);
 
-
+    // And you can disconnect like that
     my_gimbal.disconnect_gimbal();
 
     return 0;
